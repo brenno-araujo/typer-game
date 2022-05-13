d4d9effb98046f8c2e2d4170333ce7ff2b82a43e
Brenno Araújo <brennoaraujo@yahoo.com>
2022-05-13 19:42:53 -0300

    feat: timeLeft

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,27 @@
-var phrase = $('.phrase').text();
-var wordCount = phrase.split(' ').length;
-var lenghtPhrase = $("#lenghtPhrase");
+// var phrase = $('.phrase').text();
+// var wordCount = phrase.split(/\S+/).length;
+// var lenghtPhrase = $("#lenghtPhrase");
 
-lenghtPhrase.text(wordCount);
+// lenghtPhrase.text(wordCount);
+
+var textArea = $('.textArea');
+
+textArea.on('input', function() {
+    var content = textArea.val();
+    var wordCount = content.split(/\S+/).length -1;
+
+    $("#lenghtPhrase").text(wordCount);
+    $("#countCaracters").text(content.length);
+});
+
+var timeLeft = $("#digitationTime").text();
+textArea.one('focus', function() {
+    var id = setInterval(function() {
+        timeLeft--;
+        $("#digitationTime").text(timeLeft);
+        if (timeLeft == 0) {
+            textArea.attr('disabled', 'disabled');
+            clearInterval(id);
+        }
+    }, 1000);
+});
