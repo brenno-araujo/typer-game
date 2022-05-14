f29fd8ec82e4b579d87f5c3790f222bf7080c5b8
Brenno Araújo <brennoaraujo@yahoo.com>
2022-05-14 19:22:54 -0300

    refactor: functions js

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,46 @@
-// var phrase = $('.phrase').text();
-// var wordCount = phrase.split(/\S+/).length;
-// var lenghtPhrase = $("#lenghtPhrase");
-
-// lenghtPhrase.text(wordCount);
-
 var textArea = $('.textArea');
+var digitationTime = $("#digitationTime").text();
 
-textArea.on('input', function() {
-    var content = textArea.val();
-    var wordCount = content.split(/\S+/).length -1;
-
-    $("#lenghtPhrase").text(wordCount);
-    $("#countCaracters").text(content.length);
+$(document).ready(function() {
+    startCount();
+    timeLeft();
+    restart();
+    $("#restart").on('click', function() {
+        restart();
+    });
 });
 
-var timeLeft = $("#digitationTime").text();
-textArea.one('focus', function() {
-    var id = setInterval(function() {
-        timeLeft--;
-        $("#digitationTime").text(timeLeft);
-        if (timeLeft == 0) {
-            textArea.attr('disabled', 'disabled');
-            clearInterval(id);
-        }
-    }, 1000);
-});
+function startCount() {
+    textArea.on('input', function() {
+        var content = textArea.val();
+        var wordCount = content.split(/\S+/).length -1;
+    
+        $("#lenghtPhrase").text(wordCount);
+        $("#countCaracters").text(content.length);
+    });
+}
+
+function timeLeft() {  
+    var timeLeft = $("#digitationTime").text();
+    textArea.one('focus', function() {
+        var id = setInterval(function() {
+            timeLeft--;
+            $("#digitationTime").text(timeLeft);
+            if (timeLeft == 0) {
+                textArea.attr('disabled', 'disabled');
+                clearInterval(id);
+            }
+        }, 1000);
+    });
+}
+
+function restart() {
+        textArea.val('');
+        textArea.removeAttr('disabled');
+        $("#digitationTime").text(digitationTime);
+        $("#countCaracters").text("0");
+        $("#lenghtPhrase").text("0");
+        timeLeft();
+}
+
+
